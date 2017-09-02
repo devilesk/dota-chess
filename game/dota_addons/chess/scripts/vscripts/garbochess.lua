7435f36e8cdcbc6a7a205ac24cb7829b2df06667
devilesk <devilesk@gmail.com>
2017-09-02 19:38:46 -0400

    remove debug output

--- game/dota_addons/chess/scripts/vscripts/garbochess.lua
+++ game/dota_addons/chess/scripts/vscripts/garbochess.lua
@@ -704,13 +704,11 @@ end
 function IsRepDraw()
     local i = g_moveCount - 5;
     local stop = g_moveCount - 1 - g_move50;
-    PrintTable(g_repMoveStack)
-    print ("IsRepDraw", i, g_moveCount, g_move50, stop, stop < 0)
+
     stop = iif( stop < 0 , 0 , stop );
     
     while ( i >= stop ) do
         if (g_repMoveStack[1+i] == g_hashKeyLow) then
-            print("IsRepDraw g_repMoveStack[1+i] == g_hashKeyLow", 1+i, g_repMoveStack[1+i], g_hashKeyLow)
             return true;
         end
         i = i - 2;
@@ -723,13 +721,11 @@ end
 function Is3RepDraw()
     local i = g_moveCount - 5;
     local stop = g_moveCount - 1 - g_move50;
-    PrintTable(g_repMoveStack)
-    print ("IsRepDraw", i, g_moveCount, g_move50, stop, stop < 0)
+
     stop = iif( stop < 0 , 0 , stop );
     local repCount = 0
     while ( i >= stop ) do
         if (g_repMoveStack[1+i] == g_hashKeyLow) then
-            print("IsRepDraw g_repMoveStack[1+i] == g_hashKeyLow", 1+i, g_repMoveStack[1+i], g_hashKeyLow)
             repCount = repCount + 1
             if repCount == 2 then
                 return true;
@@ -1547,7 +1543,6 @@ end
 
 --
 function Search(finishMoveCallback, maxPly, finishPlyCallback)
-  print("Search", maxPly)
   local alpha = minEval;
   local beta = maxEval;
 
@@ -1958,7 +1953,6 @@ end
 
 --
 function MakeMove(move)
-    print("MakeMove start")
     local me = bit.rshift( g_toMove, 3 );
     local otherColor = 8 - g_toMove;
 
@@ -2190,17 +2184,14 @@ function MakeMove(move)
         g_inCheck = IsSquareAttackable(g_pieceList[1+ bit.lshift( bit.bor(pieceKing, g_toMove), 4)], 8-g_toMove);
     end
 
-    print("adding to g_repMoveStack", 1+(g_moveCount - 1), g_hashKeyLow)
     g_repMoveStack[1+(g_moveCount - 1)] = g_hashKeyLow;
     g_move50 = g_move50 + 1;
-    print("MakeMove end")
     return true;
 end
 --
 
 --
 function UnmakeMove(move)
-    print("UnmakeMove start")
     g_toMove = 8 - g_toMove;
     g_baseEval = -g_baseEval;
 
@@ -2298,7 +2289,6 @@ function UnmakeMove(move)
         g_pieceList[1+ bit.bor( bit.lshift(captureType , 4) , g_pieceCount[1+captureType] )] = epcEnd;
         g_pieceCount[1+captureType] = g_pieceCount[1+captureType] + 1;
     end
-    print("UnmakeMove end")
 end
 --
 
@@ -2458,7 +2448,6 @@ end
 
 --
 function GenerateValidMoves()
-    print("GenerateValidMoves start")
     local moveList = {}		-- new Array();
     local allMoves = {}		-- new Array();
     GenerateCaptureMoves(allMoves);
@@ -2471,7 +2460,6 @@ function GenerateValidMoves()
             UnmakeMove(allMoves[1+i]);
         end
     end
-    print("GenerateValidMoves end")
     return moveList;
 end
 --
